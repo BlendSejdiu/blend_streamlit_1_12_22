ae37c9a47b3649787c6525a0da88e613264d51de
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2022-12-23 22:10:04 +0100

    updated

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ else:
 def conv_lower(lista):
     lower=[]
     for i in lista:
-        if i.isupper():
-            lower.append(i)
+        lower.append(i.lower)
     print(lower)
 lista_lower = print(conv_lower(lista))
 
@@ -38,5 +37,7 @@ def lista_count(lista):
     print(count)
 numri = lista_count(lista)
 
-if st.button(" print count "):
+if st.button(" Nr "):
     st.write(numri)
+else:
+    st.write("")
